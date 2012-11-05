36927e2f5e31249304d34400eededd8d80c31f53
aolowo02 <aolowo02@cspc095.dcs.bbk.ac.uk>
2012-11-05 19:16:57 +0000

    This is for question 1 of Assignment 1

---
+++ HelloWorld/src/Hello/World.java
@@ -0,0 +1,13 @@
+package Hello;
+
+public class World {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+	}
+
+}
