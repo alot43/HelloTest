8080fc3e5137b29ad1136d28bdcc7911285b08ea
aolowo02 <aolowo02@cspc095.dcs.bbk.ac.uk>
2012-11-05 19:35:00 +0000

    Modification of Hello file changed to "Hello2"

---
+++ HelloWorld/src/Hello/Hello2.java
@@ -0,0 +1,15 @@
+package Hello;
+
+public class Hello2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Everyone");
+	}
+	
+}
+
+
---
+++ HelloWorld/src/Hello/Tola.java
@@ -0,0 +1,27 @@
+package Hello;
+
+public class Tola {
+	private String name;
+	
+	public Tola(String name) {
+		super();
+		this.name = name;
+	}
+
+	/**
+	 * @param args
+	 */
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//}
+
+}
